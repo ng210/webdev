38eaec9abab3e92a3e36697e993720c3acf4454c
gabor <gabor.nemeth.de@gmail.com>
2017-12-02 16:18:56 +0100

    add: test for parseElement and Logger deployment modified

--- src/test/test.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-setlocal EnableDelayedExpansion
-set baka=?
-:loop
-for /f "delims= tokens=1,2" %%i in (deploy.lst) do (
-    set baka=%%i
-    echo %%i.!baka!
-)
--- src/test/test.js
+++ src/test/test.js
@@ -1,32 +1,83 @@
 include('frmwrk/fw.js');
 
+var Logger = require('../utils/Logger.js');
+//include('utils/Logger.js');
+
 if (typeof onpageload === 'function') window.onload = onpageload;
 
-function onpageload(e) {
-    var content = document.getElementById('content');
+var g_logger = null;
 
-    var dummy = require('dummy.js');
-    dummy.func1();
-    content.innerHTML += '<hr/>';
-
+function loadTest(content) {
     var data = load('deploy.lst', function(data){
-        content.innerHTML += 'Data: ' + data.replace(/\n/g, '<br/>');
-        content.innerHTML += '<hr/>';
+        g_logger.info('Data: ' + data.replace(/\n/g, '<br/>'));
+        g_logger.info('<hr/>');
     }, function(error) {
-        content.innerHTML += 'Error: ' + error.message;
-        content.innerHTML += '<hr/>';
+        g_logger.error('Error: ' + error.message);
+        g_logger.info('<hr/>');
     });
 
     var data = load(['deploys.lst', 'dummy.js'], function(result){
-        content.innerHTML += result.map(function(item, ix) { return ix + ': ' + item + ';'; }).join('<br/>');
-        content.innerHTML += '<hr/>';
+        g_logger.info(result.map(function(item, ix) { return ix + ': ' + item + ';'; }).join('<br/>'));
+        g_logger.info('<hr/>');
     }, function(result) {
-        content.innerHTML += result.map(function(item, ix) { return ix + ': ' + (item.message ? item.message : item) + ';'; }).join('<br/>');
-        content.innerHTML += '<hr/>';
+        g_logger.info(result.map(function(item, ix) { return ix + ': ' + (item.message ? item.message : item) + ';'; }).join('<br/>'));
+        g_logger.info('<hr/>');
     });
+}
 
+function requireTest(content) {
+    var dummy = require('dummy.js');
+    dummy.func1();
+    g_logger.info('<hr/>');
+}
+
+function arrayTest(content) {
     var arr = new fw.Array();
     arr.push('alma');
     arr.push('szilva');
-    content.innerHTML += 'arr.class = ' + arr.getClass() + '<br/>';
+    g_logger.info('arr.class = ' + arr.getClass());
+}
+
+function loggerTest() {
+    for (var i=0; i<Logger.levels.length; i++) {
+        g_logger.setLevel(i);
+        g_logger.trace(i + ' log test');
+        g_logger.debug(i + ' log test');
+        g_logger.info(i + ' log test');
+        g_logger.warn(i + ' log test');
+        g_logger.error(i + ' log test');
+    }
+}
+
+function parseElementTest() {
+    var el = document.createElement('div');
+    el.innerHTML = load('app.cfg');
+    var cfg = parseElement(el);
+    g_logger.info('app.name: ' + cfg.app.name);
+    g_logger.info('app.title: ' + cfg.app.title);
+    g_logger.info('debug is ' + (cfg.app.debug=='0' ? 'off' : 'on'));
+}
+
+function onpageload(e) {
+    var content = document.getElementById('content');
+    g_logger = new Logger({
+        format: '<tt>#{{counter}} <b>{{level}}</b> - <i>{{file}}::{{method}}</i>({{line}}):</tt> {{message}}<br/>',
+        level: 'info',
+        print: {
+            context: null,
+            method: function(data) {
+                content.innerHTML += data;
+            }
+        }
+    });
+
+    loadTest(content);
+
+    requireTest(content);
+
+    arrayTest(content);
+
+    //loggerTest(content);
+
+    parseElementTest(content);
 }
