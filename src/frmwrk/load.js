126f9f2f992c82eb69c92f8388457ecaa56143f0
gabor <gabor.nemeth.de@gmail.com>
2017-12-05 21:09:52 +0100

    load reworked, re-added internal load function, added option contentType

--- src/frmwrk/load.js
+++ src/frmwrk/load.js
@@ -1,42 +1,58 @@
 include('frmwrk/ajax.js');
 
+// load('config.xml');
+// load(['user.xml', 'config.xml', 'web.xml']);
+// load('config.xml', onload, onerror);
+// load(['user.xml', 'config.xml', 'web.xml'], onload, onerror);
+// load({ url: 'config.xml', contentType: 'xml' });
+// load([{ url: 'user.html', contentType: 'html', method: 'get' }, { url: 'app.cfg', contentType: 'xml', method: 'post' });
+// load({ url: 'config.xml', contentType: 'xml' }, onload, onerror);
+// load([{ url: 'user.html', contentType: 'html', method: 'get' }, { url: 'app.cfg', contentType: 'xml', method: 'post' }, onload, onerror);
+
+
 function load(obj, onload, onerror) {
-    var async = typeof onload === 'function';
-    var res;
-    var context = {
-        result: []
-    };
-    if (Array.isArray(obj)) {
-        context.counter = obj.length;
-        context.onload = onload;
-        context.onerror = onerror;
-        context.error = false;
-        res = [];
-        for (var i=0; i<obj.length; i++) {
-            res[i] = ajax.send({
-                url: obj[i],
-                data: null,
-                method: 'GET',
-                async: async,
-                onload: load.onload_,
-                onerror: load.onerror_,
-                context: context,
-                index: i
-            });
-        }
+    var res = null;
+    if (!Array.isArray(obj)) {
+        res = load.load_(obj, null, -1, onload, onerror);
     } else {
-        res = ajax.send({
-            url: obj,
-            data: null,
-            method: 'GET',
-            async: async,
+        res = [];
+        context = {
+            counter: obj.length,
             onload: onload,
             onerror: onerror,
-            context: context
-        });
+            error: false,
+            result: []
+        };
+        for (var i=0; i<obj.length; i++) {
+            res[i] = load.load_(obj[i], context, i, load.onload_, load.onerror_);
+        }
     }
     return res;
 }
+load.load_ = function(item, context, ix, onload, onerror) {
+    var res = null;
+    var options = null;
+    if (typeof item === 'string') {
+        options = { url: item };
+    } else {
+        options = item;
+    }
+    for (k in load.requiredOptions_) {
+        if (options[k] === undefined) {
+            options[k] = load.requiredOptions_[k];
+        }
+    }
+    options.context = context;
+    options.index = ix;
+    res = ajax.send(options);
+    return res;
+};
+load.requiredOptions_ = {
+    url: '',
+    data: '',
+    method: 'GET',
+    contentType: 'text'
+};
 load.onload_ = function(data, xhr) {
     var context = xhr.options.context;
     context.result[xhr.options.index] = data;
