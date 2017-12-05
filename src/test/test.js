4caa6fcd76895d3a394f34a8d3fd97510ac1650f
gabor <gabor.nemeth.de@gmail.com>
2017-12-05 21:08:22 +0100

    tests extended

--- src/test/test.js
+++ src/test/test.js
@@ -8,27 +8,68 @@ if (typeof onpageload === 'function') window.onload = onpageload;
 var g_logger = null;
 
 function loadTest(content) {
-    var data = load('deploy.lst', function(data){
-        g_logger.info('Data: ' + data.replace(/\n/g, '<br/>'));
-        g_logger.info('<hr/>');
+    g_logger.info('load(\'deploy.lst\')');
+    var data = load('deploy.lst');
+    g_logger.info('<pre>' + data + '</pre>');
+    g_logger.info('load(\'deploys.lst\')');
+
+    var data = load('deploys.lst');
+    g_logger.info('<pre>' + data + '</pre>');
+
+    
+    g_logger.info('load(\'deploy.lst\', onload, onerror)...');
+    data = load('deploy.lst', function(data) {
+        g_logger.info('<pre>' + data + '</pre>');
+    
     }, function(error) {
         g_logger.error('Error: ' + error.message);
-        g_logger.info('<hr/>');
+    
+    });
+    g_logger.info('load(\'deploys.lst\', onload, onerror)...');
+    data = load('deploys.lst', function(data) {
+        g_logger.info('<pre>' + data + '</pre>');
+    
+    }, function(error) {
+        g_logger.error('Error: ' + error.message);
+    
     });
 
-    var data = load(['deploys.lst', 'dummy.js'], function(result){
-        g_logger.info(result.map(function(item, ix) { return ix + ': ' + item + ';'; }).join('<br/>'));
-        g_logger.info('<hr/>');
-    }, function(result) {
-        g_logger.info(result.map(function(item, ix) { return ix + ': ' + (item.message ? item.message : item) + ';'; }).join('<br/>'));
-        g_logger.info('<hr/>');
+    g_logger.info('load([\'deploys.lst\', \'dummy.js\'])');
+    data = load(['deploys.lst', 'dummy.js']);
+    data.forEach( function(item) {
+        g_logger.info('<pre>' + item + '</pre>');
     });
+    
+    g_logger.info('load([\'deploy.lst\', \'dummy.js\'], onload, onerror)');
+    data = load(['deploys.lst', 'dummy.js'], function(data) {
+            data.forEach( function(item) {
+                g_logger.info('<pre>' + item + '</pre>');
+            });
+        }, function(error) {
+            data.forEach( function(item, ix) {
+                if (item instanceof Error) {
+                    g_logger.info('<pre>' + ix + ': ' + error.message + '</pre>');
+                } else {
+                    g_logger.info('<pre>' + ix + ': ' + error + '</pre>');
+                }
+            });
+        
+        }
+    );
+
+    // var data = load(['deploys.lst', 'dummy.js'], function(result){
+    //     g_logger.info(result.map(function(item, ix) { return ix + ': ' + item + ';'; }).join('<br/>'));
+    // 
+    // }, function(result) {
+    //     g_logger.info(result.map(function(item, ix) { return ix + ': ' + (item.message ? item.message : item) + ';'; }).join('<br/>'));
+    // 
+    // });
 }
 
 function requireTest(content) {
     var dummy = require('dummy.js');
     dummy.func1();
-    g_logger.info('<hr/>');
+
 }
 
 function arrayTest(content) {
@@ -50,9 +91,7 @@ function loggerTest() {
 }
 
 function parseElementTest() {
-    var el = document.createElement('div');
-    el.innerHTML = load('app.cfg');
-    var cfg = parseElement(el);
+    var cfg = load('app.cfg');
     g_logger.info('app.name: ' + cfg.app.name);
     g_logger.info('app.title: ' + cfg.app.title);
     g_logger.info('debug is ' + (cfg.app.debug=='0' ? 'off' : 'on'));
@@ -61,7 +100,9 @@ function parseElementTest() {
 function onpageload(e) {
     var content = document.getElementById('content');
     g_logger = new Logger({
-        format: '<tt>#{{counter}} <b>{{level}}</b> - <i>{{file}}::{{method}}</i>({{line}}):</tt> {{message}}<br/>',
+        format: '<small><b>{{level}}</b> - <i>{{file}}::{{method}}</i>({{line}})</small> - ' +
+                '<tt>{{message}}</tt></br>',
+                //'<div style="font-size: large; background-color: silver; margin: 0px; padding: 0px"><tt>{{message}}</tt></div>',
         level: 'info',
         print: {
             context: null,
@@ -73,11 +114,11 @@ function onpageload(e) {
 
     loadTest(content);
 
-    requireTest(content);
+    //requireTest(content);
 
-    arrayTest(content);
+    //arrayTest(content);
 
     //loggerTest(content);
 
-    parseElementTest(content);
+    //parseElementTest(content);
 }
