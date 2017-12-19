cc5060bfdf18fe24d2e251044ff0bfdd70017367
gabor <gabor.nemeth.de@gmail.com>
2017-12-19 08:50:02 +0100

    removed unused file

--- dummy.js
+++
@@ -1,8 +0,0 @@
-(function(){
-    var dummy = {
-        func1: function() {
-            document.getElementById('content').innerHTML += "dummy.func1 say 'Hello world!'";
-        }
-    };
-    module.exports = dummy;
-})();
