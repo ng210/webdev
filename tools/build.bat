b11b7a4be518ad9f12df6520d8542f01a469a966
gabor <gabor.nemeth.de@gmail.com>
2017-12-19 08:49:34 +0100

    update build and deploy tools

--- tools/build.bat
+++ tools/build.bat
@@ -1,30 +1,38 @@
 @echo off
 :: Preparations
 setlocal EnableDelayedExpansion
+
+if not "%~x2" == ".js" (
+    echo Not .js file as input!
+    goto end
+)
 for %%i in (%0) do (
     set tools=%%~dpi
 )
-set tmp=%2
-:loop
-for /f "delims=\ tokens=1*" %%i in ("%tmp%") do (
-    if not "%%j"=="" (
-        if not defined source (
-            set source=%1\%%i
-        ) else (
-            set source=%source%\%%i
-        )
-    )
-    set tmp=%%j
-)
-if defined tmp goto loop
+
+REM set tmp=%2
+REM :loop
+REM for /f "delims=/\ tokens=1*" %%i in ("%tmp%") do (
+REM     if not "%%j"=="" (
+REM         if not defined source (
+REM             set source=%1\%%i
+REM         ) else (
+REM             set source=%source%\%%i
+REM         )
+REM     )
+REM     set tmp=%%j
+REM )
+REM if defined tmp goto loop
+set source=%~dp2
 
 echo *** Build js
-echo %tools%\jsbuild.exe v base=%1 in=%2 out=%3
-%tools%\jsbuild.exe v base=%1 in=%2 out=%3
+echo %tools%jsbuild.exe v base=%1 in=%2 out=%3
+%tools%jsbuild.exe v base=%1 in=%2 out=%3
 
 :: Deploy to tomcat
-echo *** Deploy to tomcat
-for /f "tokens=1,2" %%i in (%source%\deploy.lst) do (
+set deploy=%source%deploy.lst
+echo *** Deploy %deploy% to tomcat
+for /f "tokens=1,2" %%i in (%deploy%) do (
     if "%%i"=="target" (
         if not exist %%j (
             echo create directory %%j
