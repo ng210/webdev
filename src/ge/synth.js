d6a565fe6da3c108213e2c19d356b11d485e3079
gabor <gabor.nemeth.de@gmail.com>
2017-12-23 20:55:52 +0100

    synth new version using Pot

--- src/ge/synth.js
+++ src/ge/synth.js
@@ -19,11 +19,11 @@
 			// LFO
 			lfo1fre:  10,
 			lfo1amp:  11,
-			lfo1wav:  12,
+			lfo1wave: 12,
 
 			lfo2fre:  20,
 			lfo2amp:  21,
-			lfo2wav:  22,
+			lfo2wave: 22,
 
 			// env
 			env1amp:  30,
@@ -46,15 +46,15 @@
 			osc1note: 53,
 			osc1tune: 54,
 			osc1psw:  55,
-			osc1wav:  56,
+			osc1wave: 56,
 
-			osc1amp:  60,
-			osc1dc:   61,
-			osc1fre:  62,
-			osc1note: 63,
-			osc1tune: 64,
-			osc1psw:  65,
-			osc1wav:  66
+			osc2amp:  60,
+			osc2dc:   61,
+			osc2fre:  62,
+			osc2note: 63,
+			osc2tune: 64,
+			osc2psw:  65,
+			osc2wave: 66
 		},
 
 		theta: 2 * Math.PI,
@@ -112,19 +112,21 @@
 		 * Prototype of a voice for polyphony
 		 *****************************************************************************/
 		Voice: function(parent) {
-			this.gate = null;
-			this.note = null;
+			this.velocity = new ns_synth.Pot(0, 1, 0);
+			this.note = new ns_synth.Pot(0, 96, 0);
 			// create a basic synth with 2 oscillators and 2 envelopes for AM
 			this.envelopes = [ new ns_synth.Env(parent, parent.controls.env1), new ns_synth.Env(parent, parent.controls.env2) ];
 			this.lfos = [ new ns_synth.Osc(parent, parent.controls.lfo1), new ns_synth.Osc(parent, parent.controls.lfo2) ];
+			parent.controls.osc1.note = this.note; parent.controls.osc2.note = this.note;
 			this.oscillators = [ new ns_synth.Osc(parent, parent.controls.osc1), new ns_synth.Osc(parent, parent.controls.osc2) ];
 		},
 		/******************************************************************************
 		 * Prototype of a simple software synth
 		 *****************************************************************************/
-		Synth: function(smpRate, voices) {
+		Synth: function(smpRate, voiceCount) {
 			this.smpRate = smpRate;
 			this.omega = ns_synth.theta / smpRate;
+			this.nextVoice = 0;
 			// create controls
 			this.controls = {
 				amp: new ns_synth.Pot(0, 1, .8),
@@ -147,27 +149,27 @@
 				},
 				lfo1: {
 					amp: new ns_synth.Pot(0, 1, .5),
-					dc:  new ns_synth.Pot(0, 1, .5),
+					dc:  new ns_synth.Pot(0, 1, .0),
 					fre: new ns_synth.Pot(0, 1, .5),
-					note: new ns_synth.Pot(0, 1, .5),
-					tune: new ns_synth.Pot(0, 1, .5),
+					note: new ns_synth.Pot(0, 1, .0),
+					tune: new ns_synth.Pot(0, 1, .0),
 					psw: new ns_synth.Pot(0, 1, .5),
-					wave: new ns_synth.Pot(0, 1, .5),
+					wave: new ns_synth.Pot(0, 1, 1),
 				},
 				lfo2: {
 					amp: new ns_synth.Pot(0, 1, .5),
-					dc:  new ns_synth.Pot(0, 1, .5),
+					dc:  new ns_synth.Pot(0, 1, .0),
 					fre: new ns_synth.Pot(0, 1, .5),
-					note: new ns_synth.Pot(0, 1, .5),
-					tune: new ns_synth.Pot(0, 1, .5),
+					note: new ns_synth.Pot(0, 1, .0),
+					tune: new ns_synth.Pot(0, 1, .0),
 					psw: new ns_synth.Pot(0, 1, .5),
-					wave: new ns_synth.Pot(0, 1, .5),
+					wave: new ns_synth.Pot(0, 1, 1),
 				},
 				osc1: {
 					amp: new ns_synth.Pot(0, 1, .5),
 					dc:  new ns_synth.Pot(0, 1, .5),
 					fre: new ns_synth.Pot(0, 1, .5),
-					note: new ns_synth.Pot(0, 1, .5),
+					note: null,
 					tune: new ns_synth.Pot(0, 1, .5),
 					psw: new ns_synth.Pot(0, 1, .5),
 					wave: new ns_synth.Pot(0, 1, .5),
@@ -176,7 +178,7 @@
 					amp: new ns_synth.Pot(0, 1, .5),
 					dc:  new ns_synth.Pot(0, 1, .5),
 					fre: new ns_synth.Pot(0, 1, .5),
-					note: new ns_synth.Pot(0, 1, .5),
+					note: null,
 					tune: new ns_synth.Pot(0, 1, .5),
 					psw: new ns_synth.Pot(0, 1, .5),
 					wave: new ns_synth.Pot(0, 1, .5),
@@ -184,11 +186,24 @@
 			};
 			// create voices
 			this.voices = [];
-			for (var i=0; i<voices; i++) {
-				voices.push(new Voice(this));
+			for (var i=0; i<voiceCount; i++) {
+				this.voices.push(new ns_synth.Voice(this));
 			}
 		}
 	};
+	/*****************************************************************************/
+	ns_synth.Pot.prototype.set = function(value) {
+		if (value > this.min) {
+			if (value < this.max) {
+				this.value = value;
+			} else {
+				this.value = this.max;
+			}
+		} else {
+			this.value = this.min;
+		}
+		return this.value;
+	}
 	/*****************************************************************************/
 	ns_synth.Env.prototype.setGate = function(v) {
 		if (this.gate == 0) {
@@ -215,10 +230,10 @@
 				// 0.0 : 0.005s -> 1/(0*3.995 + 0.005)/smpRate = 200/smpRate
 				// 1.0 : 4.0s -> 1/(1*3.995 + 0.005)/smpRate
 				//   X : Xs -> 1/(3.995*X + 0.005)/smpRate
-				this.rate = 1/(this.parent.smpRate * (3.995 * this.atk + 0.005));
+				this.rate = 1/(this.parent.smpRate * (3.995 * this.atk.value + 0.005));
 				this.phase++;
 			case 2: // atk
-				this.timer += rate;
+				this.timer += this.rate;
 				if (this.timer >= 1.0) {
 					this.phase++;
 					this.timer = 1.0;
@@ -229,13 +244,13 @@
 				// 0.0 : 0.005s -> 1/(0*3.995 + 0.005)/smpRate = 200/smpRate
 				// 1.0 : 4.0s -> 1/(1*3.995 + 0.005)/smpRate
 				//   X : Xs -> 1/(3.995*X + 0.005)/smpRate
-				rate = 1/(this.parent.smpRate * (3.995 * this.dec + 0.005));
+				this.rate = 1/(this.parent.smpRate * (3.995 * this.dec.value + 0.005));
 				this.phase++;
 			case 4: // dec/sustain
-				if (this.timer <= this.sus) {
-					this.timer = this.sus;
+				if (this.timer <= this.sus.value) {
+					this.timer = this.sus.value;
 				} else {
-					this.timer -= rate;
+					this.timer -= this.rate;
 					//var susm1 = 1- this.sus;
 					//smp = susm1*smooth((this.timer-this.sus)/susm1) + this.sus;
 				}
@@ -244,10 +259,10 @@
 				// 0.0 :  0.005s -> 1/(0*9.995 + 0.005)/smpRate = 200/smpRate
 				// 1.0 : 10.0s -> 1/(1*9.995 + 0.005)/smpRate
 				//   X :  Xs -> 1/(9.995*X + 0.005)/smpRate
-				rate = 1/(this.parent.smpRate * (9.995 * this.rel + 0.005));
+				this.rate = 1/(this.parent.smpRate * (9.995 * this.rel.value + 0.005));
 				this.phase++;
 			case 6: // rel
-				this.timer -= rate;
+				this.timer -= this.rate;
 				if (this.timer <= 0.0) {
 					this.phase = 0;	// set to idle
 					this.timer = 0.0;
@@ -259,14 +274,14 @@
 	};
 	/*****************************************************************************/
 	ns_synth.Osc.prototype.run = function(am, fm, pm) {
-		var pitch = this.note + this.tune;
-		var delta = (this.fre + fm + ns_synth.p2f(pitch))/this.parent.smpRate;
+		var pitch = this.note.value + this.tune.value;
+		var delta = (this.fre.value + fm + ns_synth.p2f(pitch))/this.parent.smpRate;
 		if (delta >= 1.0) {
 			delta = 0.99999999;
 		}
-		var psw = pm + this.psw;
+		var psw = pm + this.psw.value;
 		var out = 0.0;
-		var wf = this.wave;
+		var wf = this.wave.value;
 		var wfc = 0;
 		if ((wf & ns_synth.WF_SIN) != 0) {
 			var arg = ns_synth.theta * this.timer;
@@ -300,9 +315,15 @@
 		if ((this.timer += delta) > 1.0) {
 			this.timer -= 1.0;
 		}
-		return this.amp*am*out;
+		return this.amp.value*am*out;
 	};
 	/*****************************************************************************/
+	ns_synth.Voice.prototype.setNote = function(note, velocity) {
+		this.note.value = note;
+		for (var i=0; i<this.envelopes.length; i++) {
+			this.envelopes[i].setGate(velocity);
+		}
+	};
 	ns_synth.Voice.prototype.run = function() {
 		// run LFOs
 		var lfo1 = 1.0;	//1.0 - this.lfos[0].amp + (this.lfos[0].amp + this.lfos[0].run(1.0, 0.0, 0.0))/2;
@@ -312,10 +333,20 @@
 		var psw = 0.0;	//this.envelopes[1].run(1.0);
 		var smp1 = this.oscillators[0].run(amp, lfo2, psw);
 		var smp2 = this.oscillators[1].run(amp, lfo2, psw);
+		return smp1 + smp2;
 	};
 	/*****************************************************************************/
 	ns_synth.Synth.prototype.setup = function(values) {
 		for (var i=0; i<values.length; i+=2) {
+			var ctrlId = values[i];
+			var key = Object.keys(ns_synth.Ctrl).find(
+				function(k) {
+					return ns_synth.Ctrl[k] == ctrlId;
+				}
+			);
+			if (key === undefined) {
+				console.log('No control with the id ' + ctrlId + ' was found!');
+			}
 			this.getControl(values[i]).value = values[i+1];
 		}
 	};
@@ -326,58 +357,59 @@
 			// LFO
 			case ns_synth.Ctrl.lfo1fre: pot = this.controls.lfo1.fre; break;
 			case ns_synth.Ctrl.lfo1amp: pot = this.controls.lfo1.amp; break;
-			case ns_synth.Ctrl.lfo1wav: pot = this.controls.lfo1.wav; break;
+			case ns_synth.Ctrl.lfo1wave: pot = this.controls.lfo1.wave; break;
 
 			case ns_synth.Ctrl.lfo2fre: pot = this.controls.lfo2.fre; break;
 			case ns_synth.Ctrl.lfo2amp: pot = this.controls.lfo2.amp; break;
-			case ns_synth.Ctrl.lfo2wav: pot = this.controls.lfo2.wav; break;
+			case ns_synth.Ctrl.lfo2wave: pot = this.controls.lfo2.wave; break;
 
 			// env
 			case ns_synth.Ctrl.env1amp: pot = this.controls.env1.amp; break;
-			case ns_synth.Ctrl.env1dc: pot = this.controls.env1.dc; break;
+			case ns_synth.Ctrl.env1dc:  pot = this.controls.env1.dc; break;
 			case ns_synth.Ctrl.env1atk: pot = this.controls.env1.atk; break;
 			case ns_synth.Ctrl.env1dec: pot = this.controls.env1.dec; break;
 			case ns_synth.Ctrl.env1sus: pot = this.controls.env1.sus; break;
 			case ns_synth.Ctrl.env1rel: pot = this.controls.env1.rel; break;
 
 			case ns_synth.Ctrl.env2amp: pot = this.controls.env2.amp; break;
-			case ns_synth.Ctrl.env2dc: pot = this.controls.env2.dc; break;
+			case ns_synth.Ctrl.env2dc:  pot = this.controls.env2.dc; break;
 			case ns_synth.Ctrl.env2atk: pot = this.controls.env2.atk; break;
 			case ns_synth.Ctrl.env2dec: pot = this.controls.env2.dec; break;
 			case ns_synth.Ctrl.env2sus: pot = this.controls.env2.sus; break;
 			case ns_synth.Ctrl.env2rel: pot = this.controls.env2.rel; break;
 
 			// osc
-			case ns_synth.Ctrl.Osc1amp: pot = this.controls.Osc1.amp; break;
-			case ns_synth.Ctrl.Osc1dc: pot = this.controls.Osc1.dc; break;
-			case ns_synth.Ctrl.Osc1fre: pot = this.controls.Osc1.fre; break;
-			case ns_synth.Ctrl.Osc1note: pot = this.controls.Osc1.note; break;
-			case ns_synth.Ctrl.Osc1tune: pot = this.controls.Osc1.tune; break;
-			case ns_synth.Ctrl.Osc1psw: pot = this.controls.Osc1.psw; break;
-			case ns_synth.Ctrl.Osc1wav: pot = this.controls.Osc1.wav; break;
+			case ns_synth.Ctrl.osc1amp: pot = this.controls.osc1.amp; break;
+			case ns_synth.Ctrl.osc1dc:  pot = this.controls.osc1.dc; break;
+			case ns_synth.Ctrl.osc1fre: pot = this.controls.osc1.fre; break;
+			case ns_synth.Ctrl.osc1note: pot = this.controls.osc1.note; break;
+			case ns_synth.Ctrl.osc1tune: pot = this.controls.osc1.tune; break;
+			case ns_synth.Ctrl.osc1psw: pot = this.controls.osc1.psw; break;
+			case ns_synth.Ctrl.osc1wave: pot = this.controls.osc1.wave; break;
 
-			case ns_synth.Ctrl.Osc1amp: pot = this.controls.Osc1.amp; break;
-			case ns_synth.Ctrl.Osc1dc: pot = this.controls.Osc1.dc; break;
-			case ns_synth.Ctrl.Osc1fre: pot = this.controls.Osc1.fre; break;
-			case ns_synth.Ctrl.Osc1note: pot = this.controls.Osc1.note; break;
-			case ns_synth.Ctrl.Osc1tune: pot = this.controls.Osc1.tune; break;
-			case ns_synth.Ctrl.Osc1psw: pot = this.controls.Osc1.psw; break;
-			case ns_synth.Ctrl.Osc1wav: pot = this.controls.Osc1.wav; break;
+			case ns_synth.Ctrl.osc2amp: pot = this.controls.osc2.amp; break;
+			case ns_synth.Ctrl.osc2dc:  pot = this.controls.osc2.dc; break;
+			case ns_synth.Ctrl.osc2fre: pot = this.controls.osc2.fre; break;
+			case ns_synth.Ctrl.osc2note: pot = this.controls.osc2.note; break;
+			case ns_synth.Ctrl.osc2tune: pot = this.controls.osc2.tune; break;
+			case ns_synth.Ctrl.osc2psw: pot = this.controls.osc2.psw; break;
+			case ns_synth.Ctrl.osc2wave: pot = this.controls.osc2.wave; break;
+			default: console.log('The control id ' + controlId + ' is invalid!'); break
 		}
 		return pot;
 	};
 	ns_synth.Synth.prototype.setNote = function(note, velocity) {
-		this.controls.osc1.node = node;
-		this.controls.osc1.node = node;
-		for (var i=0; i<this.envelopes.length; i++) {
-			this.envelopes[i].setGate(velocity);
-		}
+		// get free voice
+		var voice = this.voices[this.nextVoice];
+		//console.log('voice #' + this.nextVoice + ' - ' + note + '/' + velocity);
+		this.nextVoice = (this.nextVoice + 1) % this.voices.length;
+		voice.setNote(note, velocity);
 	};
 	ns_synth.Synth.prototype.run = function(buffer, start, end) {
 		for (var i=start; i<end; i++) {
 			var smp = 0;
-			for (var i=0; i<this.voices.length; i++) {
-				smp += this.voices[i].run();
+			for (var j=0; j<this.voices.length; j++) {
+				smp += this.voices[j].run();
 			}
 			buffer[i] = smp;
 		}
--- src/synth/Pot.js
+++ src/synth/Pot.js
@@ -1,78 +1,76 @@
 (function(){
-    function Pot(el, synth) {
-        el.pot = this;
-        // el.onmouseover = Pot.onmouseover;
-        // el.onmouseout = Pot.onmouseout;
-        // el.onclick = Pot.onclick;
-        el.onmousedown = Pot.onmousedown;
-        el.onmouseup = Pot.onmouseup;	this.min = parseFloat(el.min) || 0;
-    
-        this.el = el;
-        var min = parseFloat(el.getAttribute('min')) || 0;
-        var max = parseFloat(el.getAttribute('max')) || 1;
-        var value = parseFloat(el.getAttribute('value')) || .5;
-        this.scale = parseFloat(el.getAttribute('scale')) || 100;
-        this.synth = synth;
-        var id = this.el.id.split('_')[1];
-        var pot = synth.getControl(ns_synth.Ctrl[id]);
-        if (pot != null) {
-            this.pot = pot;
-            this.pot.min = min;
-            this.pot.max = max;
-            this.pot.value = value;
-        } else {
-            console.log('Unknown control "' + id + '"');
+    Object.defineProperty(global, 'Pot', {
+        "enumerable": false,
+        "configurable": false,
+        "writable": false,
+        "value": {
+            "selectedItem": null,
+            "oldOnMouseMove": null,
+            "oldOnMouseUp": null,
+            "dragPoint": [0, 0],
+            "dragSpeed": 0.5,
+        
+            "onmousedown": function(e) {
+                if (Pot.selectedItem != null) {
+                    Pot.selectedItem.style.border = 'none';
+                }
+                Pot.selectedItem = e.target;
+                Pot.dragPoint[0] = e.screenX;
+                Pot.dragPoint[1] = e.screenY;
+                Pot.selectedItem.style.border = 'solid 1px #4060a0';
+                Pot.oldOnMouseMove = document.onmousemove;
+                Pot.oldOnMouseUp = document.onmouseup;
+                document.onmousemove = Pot.onmousemove;
+                document.onmouseup = Pot.onmouseup;
+                e.preventDefault();
+            },
+            "onmouseup": function(e) {
+                document.onmousemove = Pot.oldOnMouseMove;
+                document.onmouseup = Pot.oldOnMouseUp;
+                e.preventDefault();
+            },
+            "onmousemove": function(e) {
+                //var deltaX = e.screenX - Pot.dragPoint[0];
+                var deltaY = e.screenY - Pot.dragPoint[1];
+                var sgn = deltaY < 0 ? 1 : -1;
+                //Pot.dragPoint[0] = e.screenX;
+                Pot.dragPoint[1] = e.screenY;
+                
+                var pot = Pot.selectedItem.pot;
+                var delta = Pot.dragSpeed/Pot.selectedItem.scale * sgn * deltaY*deltaY;
+                var value = pot.set(pot.value + delta);
+                Pot.selectedItem.innerHTML = Math.floor(value * Pot.selectedItem.scale);
+            },
+            "bind": function(el, synth) {
+                // el.onmouseover = Pot.onmouseover;
+                // el.onmouseout = Pot.onmouseout;
+                // el.onclick = Pot.onclick;
+                el.onmousedown = Pot.onmousedown;
+                el.onmouseup = Pot.onmouseup;
+            
+                var min = parseFloat(el.getAttribute('min')) || 0;
+                var max = parseFloat(el.getAttribute('max')) || 1;
+                var value = parseFloat(el.getAttribute('value'));
+                var ctrlId = el.getAttribute('bind');
+                el.scale = parseFloat(el.getAttribute('scale')) || 100;
+                var pot = synth.getControl(ns_synth.Ctrl[ctrlId]);
+                if (pot != null) {
+                    el.pot = pot;
+                    pot.min = min;
+                    pot.max = max;
+                    if (isNaN(value)) {
+                        value = pot.value;
+                    } else {
+                        value = pot.set(value);
+                    }
+                    el.innerHTML = Math.floor(value * el.scale);
+                    
+                } else {
+                    console.log('Unknown control "' + ctrlId + '"');
+                }                
+            },
+
         }
-    }
-    
-    Pot.prototype.set = function(value) {
-        if (value < this.min) {
-            value = this.min;
-        }
-        if (value > this.max) {
-            value = this.max;
-        }
-        this.el.innerHTML = Math.floor(value * this.scale);
-        this.synth.setControl(this.ctrlId, value);
-    }
-    Pot.selectedItem = null;
-    Pot.oldOnMouseMove = null;
-    Pot.oldOnMouseUp = null;
-    Pot.dragPoint = [0, 0];
-    Pot.dragSpeed = 0.5;
-    // Pot.onmouseover = function(e) {
-    // 	e.pot.onmouseover(e);
-    // };
-    // Pot.onmouseout = function(e) {
-    // 	e.pot.onmouseover(e);
-    // };
-    Pot.onmousedown = function(e) {
-        if (Pot.selectedItem != null) {
-            Pot.selectedItem.style.border = 'none';
-        }
-        Pot.selectedItem = e.target;
-        Pot.dragPoint[0] = e.screenX;
-        Pot.dragPoint[1] = e.screenY;
-        Pot.selectedItem.style.border = 'solid 1px #4060a0';
-        Pot.oldOnMouseMove = document.onmousemove;
-        document.onmousemove = Pot.onmousemove;
-        document.onmouseup = Pot.onmouseup;
-        e.preventDefault();
-    };
-    Pot.onmouseup = function(e) {
-        document.onmousemove = Pot.oldOnMouseMove;
-        document.onmouseup = Pot.oldOnMouseUp;
-        e.preventDefault();
-    };
-    Pot.onmousemove = function(e) {
-        //var deltaX = e.screenX - Pot.dragPoint[0];
-        var deltaY = e.screenY - Pot.dragPoint[1];
-        var sgn = deltaY < 0 ? 1 : -1;
-        //Pot.dragPoint[0] = e.screenX;
-        var pot = Pot.selectedItem.pot;
-        Pot.dragPoint[1] = e.screenY;
-        var delta = Pot.dragSpeed/pot.scale * sgn * deltaY*deltaY;
-        pot.set(pot.synth.getControl(pot.ctrlId) + delta);
-    };
+    });
     module.exports=Pot;    
 })();
