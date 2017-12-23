937613bfb28ca0caecc35867f222b2320474ae26
gabor <gabor.nemeth.de@gmail.com>
2017-12-23 20:55:26 +0100

    global symbol added

--- src/base/globals.js
+++ src/base/globals.js
@@ -1,14 +1,24 @@
-Object.defineProperties(window.top, {
-    module: {
-        enumerable: false,
-        configurable: false,
-        writable: false,
-        value: {}
-    },
-    include: {
+if (window.top) {
+    Object.defineProperties(window.top, {
+        "module": {
+            "enumerable": false,
+            "configurable": false,
+            "writable": false,
+            "value": {}
+        },
+        "global": {
+            "enumerable": false,
+            "configurable": false,
+            "writable": false,
+            "value": window.top
+        }
+    });
+}
+Object.defineProperties(global, {
+    "include": {
         enumerable: false,
         configurable: false,
         writable: false,
         value: function(path) {}
-    },
+    }
 });
