b6fa8714b8479447262a0faee1b83628c114188d
gabor <gabor.nemeth.de@gmail.com>
2017-12-24 21:54:13 +0100

    build & deploy changed

--- tools/build.bat
+++ tools/build.bat
@@ -2,10 +2,11 @@
 :: Preparations
 setlocal EnableDelayedExpansion
 
-if not "%~x2" == ".js" (
-    echo Not .js file as input!
-    goto end
-)
+REM if not "%~x2" == ".js" (
+REM     echo Not .js file as input!
+REM     goto end
+REM )
+
 for %%i in (%0) do (
     set tools=%%~dpi
 )
@@ -23,16 +24,16 @@ REM     )
 REM     set tmp=%%j
 REM )
 REM if defined tmp goto loop
+set base=%1
 set source=%~dp2
 
-echo *** Build js
-echo %tools%jsbuild.exe v base=%1 in=%2 out=%3
-%tools%jsbuild.exe v base=%1 in=%2 out=%3
+REM echo *** Build js
+REM echo %tools%jsbuild.exe v base=%1 in=%2 out=%3
+REM %tools%jsbuild.exe v base=%1 in=%2 out=%3
 
 :: Deploy to tomcat
 set deploy=%source%deploy.lst
-echo *** Deploy %deploy% to tomcat
-for /f "tokens=1,2" %%i in (%deploy%) do (
+for /f "tokens=1,2*" %%i in (%deploy%) do (
     if "%%i"=="target" (
         if not exist %%j (
             echo create directory %%j
@@ -41,11 +42,17 @@ for /f "tokens=1,2" %%i in (%deploy%) do (
         set target_=%%j
         echo Changed target to !target_!
     ) else (
-        if "%%i"=="copy" (
-            echo deploy %1\%%j
-            copy /y %1\%%j !target_! >> log.txt
-        )
-    )
+    if "%%i"=="copy" (
+        echo deploy %1\%%j
+        copy /y %1\%%j !target_! >> log.txt
+    ) else (
+    if "%%i"=="run" (
+        set _t1=%%j %%k
+        set _t2=!_t1:{tools}=%tools%!
+        set _t1=!_t2:{base}=%base%\src!
+        echo Exeute !_t1!
+        call !_t1!
+    )))
 )
 if exist log.txt del log.txt
 
