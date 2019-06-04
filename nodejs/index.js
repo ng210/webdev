49fa35ef426b0d4337a9f5865ad9c3d3431c475a
ng210 <gabor.nemeth.de@gmail.com>
2019-06-05 00:34:17 +0200

    Default nodejs app

---
+++ nodejs/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+app.use(express.json());
+const port = process.env.PORT || 3000;
+
+const documentPath = 'c:/code/git/webdev';
+
+app.get('/*', function(req, resp) {
+    var resPath = path.normalize(path.join(documentPath, req.path));
+    var exists = fs.existsSync(resPath);
+    if (exists) {
+        if (fs.statSync(resPath).isDirectory()) {
+            resPath += 'index.html';
+        }
+        if ((exists = fs.existsSync(resPath)) == true)  {
+            //console.log(`requested: ${resPath}`);
+            resp.sendFile(resPath);
+        }
+    }
+    if (!exists) {
+        resp.status(404).sendFile(`${__dirname}/404.html`);
+    }
+});
+
+
+console.log(`server is listening on ${port}`);
+app.listen(port);
