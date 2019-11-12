956ef54b51991baf0db8d9c8b19e53e02ef30bf6
ng210 <31445817+ng210@users.noreply.github.com>
2019-11-12 16:30:31 +0100

    Draft 2.0

--- src/ui/idataseries.js
+++ src/ui/idataseries.js
@@ -1,59 +1,84 @@
  (function() {
-    // Data series are 2 dimensional, enumerable structures.
-    // The first dimension/index selects the series, the second
-    // selects a value within the selected series.
-    // A value may not exist at the requested index.
-    // The series may not share a common range.
-    // The 3 main methods are 'get' and 'set' and 'getRange'.
-    //
-    // Array get(seriesId, x)
-    // Returns every value stored at the selected x within
-    // the requested series or null.
-    // - the selected series is iterated to locate entries with
-    //   the given x
-    // - the values of the matching entries are added to the
-    //   result along with x to form [x,y] pairs (data point)
-    //
-    // Value set(seriesId, x, y)
-    // This method sets, modifies or removes an entry at the
-    // requested x within the selected series and Returns the
-    // old value.
-    // - the selected series is iterated to locate entries with
-    //   the given x and y value
-    // - if an entry was
-    //   - found, it will be removed
-    //   - not found it will be set or modified
-    // - set and modify are distinguished only by looking at the
-    //   internal data structure: an existing value at x is
-    //   modified otherwise a new value is set (y != value at x)
-    // - The series may allow or deny the storage of multiple
-    //   values at the same x. If multiple values are allowed,
-    //   the 'get' method can return more than one data points
-    //   and 'getRange' can return multiple entries with the
-    //   same x.
-    //
-    // Info getRange(seriesId, start, end, data)
-    // Adds data points with x values between start and end
-    // to the 'data' variable as a flat Array, and returns
-    // information about the range.
-    // - The information includes
-    //   - count: count of data points within the range
-    //   - minX: the smallest x of found points
-    //   - maxX: the biggest x of found points
-    //   - minY: the smallest y of found points
-    //   - maxY: the biggest y of found points
-    // - The points in 'data' are sorted by x.
-    // interpolation, step
-    //   
-    //
-    // Default implementation is for arrays of simple values (Number, String, ...) containing 1 series only
-    // Example: enumerable =[ [1, 3, 6, 7, 9, 10, ...] ] or { 'values': [0.1, 0.4, 0.8, 0.3, -0.2] }
-    // - get returns value at index
-    // - set clears or modifies value at index
+/******************************************************************************
+ Data series are 2 dimensional, enumerable structures. The first dimension is
+ the series, the second dimension indexes a value within the selected series.
+ - A value may not exist at the requested index.
+ - The series may not share a common range.
+ The 3 main methods are 'get' and 'set' and 'getRange'.
+ 
+ Array get(seriesId, x)
+ Returns every value stored at the selected x within the requested series as
+ an array or null.
+ - The selected series is iterated to locate entries with the given x.
+ - The values of the matching entries are added to the result along with the x
+   to form [x,y] pairs (data points). These pairs are written as 2 numbers.
+ - The method returns null if a series identified by 'seriesId' or a value at x
+   cannot be found.
+ 
+ Value set(seriesId, x, y)
+ This method sets, modifies or removes an entry at the requested x within the
+ selected series and eventually returns the old value.
+ - The selected series is searched for entries with the given x and y value.
+ - If an entry was
+     - found, it will be removed or
+     - not found it will be set or modified
+ - Set and modify are distinguished only by looking at the internal structure:
+   - value at x does not match y, the value is modified to y,
+   - value at x is different from y, a new value is added and set to y.
+ - A series may allow or deny the storage of multiple values at the same x.
+   - With multiple values allowed, the 'get' method can return more than one
+     data point and 'getRange' can return multiple values with the same x.
+ 
+ Values getRange(seriesId, Info)
+ Returns an array of data points, selected by parameters in the Info structure.
+ The data points are written as 2 values x and y.
+ as a flat Array, and returns information about the range.
+ - The information includes 3 input parameters
+     - step: size of step along the x axis,
+     - start: start of the range of x values,
+     - end: end of the range of x values,
+   and 3 output parameters
+     - count: count of data points within the range,
+     - min: a data point with the smallest x and y within the range,
+     - max: a data point with the biggest x and y within the range,
+ - The points in 'data' are sorted by x ascending.
+ - The iteration tries to get a value from the selected series for each x.
+ - If a value could be retrieved it is stored in the output array as a data
+   point with x and y.
+ - The count of minimum and maximum of x and y are stored in the info.
+******************************************************************************/
     function IDataSeries(enumerable) {
         this.data = enumerable || [ [] ];
-        this.step = 1;
+        this.info = {};
     }
+
+    // The default implementation assumes a data source of a single series
+    // containing simple values only, thus the seriesId is always 0.
+    // Examples:
+    //   [ [1, 3, 6, 7, 9, 10, ...] ] or
+    //   { 'values': [0.1, 0.4, 0.8, 0.3, -0.2] }
+
+    // Set information about a series: title, multiple.
+    IDataSeries.prototype.setInfo = function(seriesId, info) {
+        this.info[seriesId] = info;
+    };
+
+    IDataSeries.prototype.get = function(seriesId, ix) {
+        var series = this.data[seriesId];
+        return (series && series[ix] != undefined) ? [ix, series[ix]] : null;
+    };
+
+	IDataSeries.prototype.set = function(seriesId, ix, value) {
+        if (this.data[seriesId] == undefined) {
+            this.data[seriesId] = [];
+        }
+        var series = this.data[seriesId];
+        if (!this.info[seriesId].multiple && series[ix] != undefined) {
+            this.remove(seriesId, ix);
+        }
+        series[ix] = value;
+    };
+
 	IDataSeries.prototype.getRange = function(seriesId, start, end, data) {
         var info = { count:0, min:null, max:null };
         if (this.data && this.data[seriesId] != undefined) {
@@ -71,24 +96,14 @@
         }
         return info;
     };
-	IDataSeries.prototype.get = function(seriesId, ix) {
-        return this.data[seriesId][ix] != undefined ? [ix, this.data[seriesId][ix]] : null;
-    };
-	IDataSeries.prototype.set = function(seriesId, ix, value) {
-        if (this.data[seriesId] == undefined) {
-            this.data[seriesId] = [];
+
+	// IDataSeries.prototype.add = function(index, chnId) { throw new Error('Not implemented!'); };
+	IDataSeries.prototype.remove = function(seriesId, ix, value) {
+        var series = this.data[seriesId];
+        if (series != undefined) {
+            series.splice(ix, 1);
         }
-        // modify or clear value at index
-        this.data[seriesId][ix] = this.data[seriesId][ix] != value ? value : undefined;
     };
-    IDataSeries.prototype.push = function(seriesId, value) {
-        if (this.data[seriesId] == undefined) {
-            this.data[seriesId] = [];
-        }
-        this.set(seriesId, this.data[seriesId].length, value);
-    };
-	IDataSeries.prototype.add = function(index, chnId) { throw new Error('Not implemented!'); };
-	IDataSeries.prototype.remove = function(index, chnId) { throw new Error('Not implemented!'); };
 
     public(IDataSeries, 'IDataSeries');
 })();
