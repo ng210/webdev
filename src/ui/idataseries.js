c3e8ac46f79cf65f7f224a94f69a8dcff964cbd4
ng210 <31445817+ng210@users.noreply.github.com>
2019-11-12 11:07:06 +0100

    Draft 1.0

--- src/ui/idataseries.js
+++ src/ui/idataseries.js
@@ -1,7 +1,51 @@
  (function() {
-
-    // The enumerable is sorted by its index (i.e. Object or Array)
-    // Arrays are always sorted (i < j if a[i] comes before a[j])
+    // Data series are 2 dimensional, enumerable structures.
+    // The first dimension/index selects the series, the second
+    // selects a value within the selected series.
+    // A value may not exist at the requested index.
+    // The series may not share a common range.
+    // The 3 main methods are 'get' and 'set' and 'getRange'.
+    //
+    // Array get(seriesId, x)
+    // Returns every value stored at the selected x within
+    // the requested series or null.
+    // - the selected series is iterated to locate entries with
+    //   the given x
+    // - the values of the matching entries are added to the
+    //   result along with x to form [x,y] pairs (data point)
+    //
+    // Value set(seriesId, x, y)
+    // This method sets, modifies or removes an entry at the
+    // requested x within the selected series and Returns the
+    // old value.
+    // - the selected series is iterated to locate entries with
+    //   the given x and y value
+    // - if an entry was
+    //   - found, it will be removed
+    //   - not found it will be set or modified
+    // - set and modify are distinguished only by looking at the
+    //   internal data structure: an existing value at x is
+    //   modified otherwise a new value is set (y != value at x)
+    // - The series may allow or deny the storage of multiple
+    //   values at the same x. If multiple values are allowed,
+    //   the 'get' method can return more than one data points
+    //   and 'getRange' can return multiple entries with the
+    //   same x.
+    //
+    // Info getRange(seriesId, start, end, data)
+    // Adds data points with x values between start and end
+    // to the 'data' variable as a flat Array, and returns
+    // information about the range.
+    // - The information includes
+    //   - count: count of data points within the range
+    //   - minX: the smallest x of found points
+    //   - maxX: the biggest x of found points
+    //   - minY: the smallest y of found points
+    //   - maxY: the biggest y of found points
+    // - The points in 'data' are sorted by x.
+    // interpolation, step
+    //   
+    //
     // Default implementation is for arrays of simple values (Number, String, ...) containing 1 series only
     // Example: enumerable =[ [1, 3, 6, 7, 9, 10, ...] ] or { 'values': [0.1, 0.4, 0.8, 0.3, -0.2] }
     // - get returns value at index
