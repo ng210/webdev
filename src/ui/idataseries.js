f4124c9924ab466480d53cbaf76643b45d5dec71
ng210 <31445817+ng210@users.noreply.github.com>
2020-01-20 09:22:42 +0100

    Documentation 1.

--- src/ui/idataseries.js
+++ src/ui/idataseries.js
@@ -5,52 +5,19 @@
         return result;
     }
 /******************************************************************************
- Data series are 2 dimensional, enumerable structures. The first dimension is
- the series, the second dimension indexes a value within the selected series.
- - A value may not exist at the requested index.
- - The series may not share a common range.
- The 3 main methods are 'get', 'set' and 'getRange'.
+ Data series are at least 2 dimensional, enumerable, sortable structures. It is
+ a list of vectors (x, y[, ...]). The list can be sorted by x to improve search
+ performance.
+ A value may not exist at the requested index.
+ Supported methods
+ - data get(x)
+ - set(x, value)
+ - removeAt(ix)
+ - data getRange(range)
+ - info getInfo()
+ - point getAsPoint(ix, value)
+ - point getByIndex(ix)
  
- Array get(seriesId, x)
- Returns every value stored at the selected x within the requested series as
- an array or null.
- - The selected series is iterated to locate entries with the given x.
- - The values of the matching entries are added to the result along with the x
-   to form [x,y] pairs (data points). These pairs are written as 2 numbers.
- - The method returns null if a series identified by 'seriesId' or a value at x
-   cannot be found.
- 
- Value set(seriesId, x, y)
- This method sets, modifies or removes an entry at the requested x within the
- selected series and eventually returns the old value.
- - The selected series is searched for entries with the given x and y value.
- - If an entry was
-     - found, it will be removed or
-     - not found it will be set or modified
- - Set and modify are distinguished only by looking at the internal structure:
-   - value at x does not match y, the value is modified to y,
-   - value at x does not exist, a new value is added and set to y.
- - A series may allow or deny the storage of multiple values at the same x.
-   - With multiple values allowed, the 'get' method can return more than one
-     data point and 'getRange' can return multiple values with the same x.
- 
- Values getRange(seriesId, Info)
- Returns an array of data points, selected by parameters in the Info structure.
- Each data point is written as 2 values (x and y) and the function also returns
- information about the range.
- - The information includes 3 input parameters
-     - step: size of step along the x axis,
-     - start: start of the range of x values,
-     - end: end of the range of x values,
-   and 3 output parameters
-     - count: count of data points within the range,
-     - min: a data point with the smallest x and y within the range,
-     - max: a data point with the biggest x and y within the range,
- - The points in 'Values' are sorted by x ascending.
- - The iteration tries to get a value from the selected series for each x.
- - If a value could be retrieved it is stored in the output array as a data
-   point with x and y.
- - The count of minimum and maximum of x and y are stored in the info.
 ******************************************************************************/
     function IDataSeries(enumerable) {
         this.data = enumerable || [];
@@ -141,9 +108,12 @@
         this.data.splice(start, ix - start);
     };
 
-    // The default implementation assumes a series as a simple array.
+    // The default implementation uses a simple array of numbers.
+    // The index is used as x and the stored value as y.
+    // Since array indexing is continuous, unassigned indices store
+    // undefined.
     // Examples:
-    //   [1, 3, 6, 7, 9, 10, ...] or
+    //   [1, 3, 6, undefined, 7, 9, 10, ...] or
     //   [0.1, 0.4, 0.8, 0.3, -0.2]
     IDataSeries.prototype.getAsPoint = function(ix) {
         return { x: ix, y: this.data[ix] };
