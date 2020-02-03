0fe14bfd2bb99ce85aca6ab8f4579e6c183589ba
ng210 <31445817+ng210@users.noreply.github.com>
2020-02-03 09:42:24 +0100

    synthAdapter.fromDataseries added

--- src/synth/synth-adapter-ext.js
+++ src/synth/synth-adapter-ext.js
@@ -98,12 +98,41 @@ psynth.SynthAdapter.toDataSeries = function(sequence) {
 
 psynth.SynthAdapter.fromDataSeries = function(series, channelId) {
     var sequence = null;
-    var ds = series[channelId];
-    if (ds) {
-        sequence = new Sequence();
-        for (var i=0; i<ds.data.length; i++) {
-            
+    var keys = Object.keys(series);
+    var f0 = 0, f1 = 0;
+    var noteMap = [];
+    while (true) {
+        for (var k=0; k<keys.length; k++) {
+            var ds = series[k];
+            var data = ds.get(f0);
+            if (data) {
+                if (sequence == null) {
+                    sequence = new Sequence();
+                }
+                sequence.stream.writeUint16(f0 - f1);
+                f1 = f0;
+                if (k == synth.SynthAdapter.SETNOTE) {
+                    sequence.stream.writeUint8(k);
+                    var note = data[1];
+                    sequence.stream.writeUint8(note);
+                    sequence.stream.writeUint8(data[2]);
+                    noteMap[note] = f0 + data[3];
+                } else {
+                    // todo: handle other commands
+                    sequence.stream.writeUint8(psynth.SynthAdapter.SETCTRL8);
+                    sequence.stream.writeUint8(k);
+                    sequence.stream.writeUint8(data[1]);
+                }
+            }
         }
+        for (var n in noteMap) {
+            if (noteMap[n] == f0) {
+                sequence.stream.writeUint8(psynth.SynthAdapter.SETNOTE);
+                sequence.stream.writeUint8(n);
+                sequence.stream.writeUint8(0);
+            }
+        }
+        sequence.stream.writeUint8(Player.EOF);
     }
     return sequence;
 };
