4daa5f1c072d431bc06237de618b83da37d7c1d6
ng210 <nemeth_g@freemail.hu>
2020-02-19 20:38:54 +0100

    Changed testing framework - added methods test and print

--- src/test/test.js
+++ src/test/test.js
@@ -1,5 +1,28 @@
 include('/base/dbg.js');
 
+//var _indentText = ['&nbsp;', '&nbsp;', '&nbsp;', '&nbsp;', '&nbsp;', '&nbsp;', '&nbsp;', '&nbsp;', '&nbsp;', '&nbsp;', '&nbsp;', '&nbsp;', '&nbsp;'];
+var _indentText = '        ';
+var _bulletinSymbols = ['►', '▪', '∙'];
+
+function print(node, indent) {
+    if (Array.isArray(node)) {
+        for (var i=0; i<node.length; i++) {
+            print(node[i], indent+1);
+        }
+    } else {
+        Dbg.prln(`<pre>${_indentText.substr(0, indent)}</pre>${_bulletinSymbols[indent<_bulletinSymbols.length ? indent-1 : _bulletinSymbols.length-1]} ${node}`);
+    }
+}
+
+function test(lbl, action) {
+    var errors = action();
+    var result = [lbl + '..' + (errors ? '<span style="color:#ff4040">Failed</span>' : '<span style="color:#40ff40">Ok</span>')];
+    if (errors) {
+        result.push(errors);
+    }
+    return result;
+}
+
 async function onpageload(errors) {
     Dbg.init('con');
     Dbg.prln('Tests 0.1');
@@ -7,7 +30,6 @@ async function onpageload(errors) {
 
     var url = new Url(location.href);
     var testUrl = new Url(`${url.fragment}/test.js`);
-
     Dbg.prln(`Load '${testUrl}'`);
     var module = await load(testUrl.toString());
     await load(new Url(`${url.fragment}/test.css`).toString());
@@ -15,9 +37,9 @@ async function onpageload(errors) {
         var testName = Object.keys(module.symbols)[0];
         var test = module.symbols[testName];
         if (typeof test === 'function') {
-            Dbg.prln(`Running '${testName}'...`);
-            var ret = await test();
-            Dbg.prln(`Test returned ${ret}`, 1);
+            Dbg.prln(`<b>Running '<i>${testName}</i>'...</b>`);
+            print(await test(), -1);
+            Dbg.prln('<b>Test finished</b>');
         }
     } else {
         Dbg.prln(`Error loading ${module.error}`);
