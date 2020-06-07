00efd18d4317b4cf16af5fbbdea8c4da4150a5ae
ng210 <nemeth_g@freemail.hu>
2020-06-07 12:24:48 +0200

    Fixed worker test

--- js/worker-test.js
+++ js/worker-test.js
@@ -4,6 +4,7 @@ include('/synth/synth.js');
 
     function main() {
         Dbg.prln('MAIN');
+        self.postMessage({code:'kill', id:0, data:`Worker terminated!`});
     }
 
     public(main, 'main');
