229ea6892541e460ed9c6d64dee37093c233d4a5
ng210 <nemeth_g@freemail.hu>
2020-07-08 22:11:56 +0200

    fire demo completed

--- js/demo/fire/fire.js
+++ js/demo/fire/fire.js
@@ -1,15 +1,16 @@
 include('ge/fn.js');
+include('ge/noise.js');
 
 (function() {
 	
     var Fire = {
 		name: 'Fire',
 		settings: {
-			images: { label: 'Image', value: 'lens.gif', type: 'string', link: null },
-			motion: { label: 'Motion', value: 0.7, min:0, max:1, step: 0.01, type: 'float', link: null },
-			feedback: { label: 'Feedback', value: 0.7, min:0, max:1, step: 0.01, type: 'float', link: null },
-			cooling: { label: 'Cooling', value: 0.05, min:0, max:1, step: 0.01, type: 'float', link: null },
-			color: { label: 'Color', value: 0, type: 'int', link: null }
+			images: { label: 'Image', value: 'fire.png', type: 'string', link: null },
+			motion: { label: 'Motion', value: 0.4, min:0, max:1.0, step: 0.005, normalized:true, type: 'float', link: null },
+			feedback: { label: 'Feedback', value: 0.7, min:0, max:2.0, step: 0.005, normalized:true, type: 'float', link: null },
+			cooling: { label: 'Cooling', value: 0.1, min:0, max:0.4, step: 0.005, normalized:true, type: 'float', link: null },
+			color: { label: 'Color', value: 0, min:0, max:1, step: 1,type: 'int', link: null }
 		},
 
 		fbmSize: 50,
@@ -23,18 +24,19 @@ include('ge/fn.js');
 		frontBuffer: { context:null, buffer: null },
 		backBuffer: { context:null, buffer: null },
 		mode: 0,
+		noise: null,
 		filter: new Fn.Filter([
-			[0.00, 0.10, 0.00],
-			[0.07, 0.30, 0.07],
-			[0.10, 0.70, 0.10]
+			[ 0,  0,  0],
+			[ 1,  3,  1],
+			[ 3, 7,  3]
 		]),
 
-        initialize: async function initialize() {
+		initialize: async function initialize() {
 			// create list of images
 			var urls = [
-				'/demo/lens/lens.gif', '/demo/lens/deepspace.jpg',
-				'/demo/fire/fire.gif', '/demo/rotozoom/javascript.gif',
-				'/test/test.gif'
+				'/demo/data/fire.png',
+				'/demo/data/bump.gif',
+				'/demo/data/javascript.gif'
 			];
 			var res = await load(urls);
 			for (var i=0; i<res.length; i++) {
@@ -66,72 +68,84 @@ include('ge/fn.js');
 			ctrl.column.cells[rowColumn[1]] = combobox;
 			DemoMgr.controls.settings.remove(ctrl);
 			DemoMgr.controls.settings.add(combobox);
+
+			this.noise = new Noise();
 			//DemoMgr.controls.settings.rows["images"].cells["value"].setItems(this.images.map(v => v.alt));
 			var canvas = document.createElement('canvas');
 			canvas.width = Math.floor(glui.width/4);
 			canvas.height = Math.floor(glui.height/4);
+			canvas.style.backgroundColor = 'rgb(0, 0, 0)';
 			this.frontBuffer.context = canvas.getContext('2d');
 			this.frontBuffer.buffer = this.frontBuffer.context.getImageData(0, 0, canvas.width, canvas.height);
 			var canvas = document.createElement('canvas');
+			canvas.style.backgroundColor = 'rgb(0, 0, 0)';
 			this.backBuffer.context = canvas.getContext('2d');
-debugger
 			this.setImage();
 			this.heatMaps[1] = this.createHeatMap();
 			this.update(0);
         },
         resize: function resize(e) {
-        },
+		},
+		maxHeat: 0,
         update: function update(frame, dt) {
 			var id = frame % 2;
 			var source = this.heatMaps[id];
 			var target = this.heatMaps[1 - id];
 			var ix = 0;
 			var cooling = 1 - this.settings.cooling.value;
+			var jx = 0;
 			for (var j=0; j<source.height; j++) {
 				for (var i=0; i<source.width; i++) {
-					var v = this.filter.apply(source.data, source.width, source.height, 1, i, j, 0);
-					var motion = Fn.lerp(1.0, Math.random(), this.settings.motion.value);
-					var heat = this.settings.feedback.value * motion * this.heatMaps[2].data[ix];
+					var avg = this.filter.apply(source.data, source.width, source.height, 1, i, j, 0);
+					var r = this.noise.fbm3d(4*i/source.width, 4*j/source.height, 0.01*this.settings.motion.value*frame, 3, 0.5, 4.01, 1.3, 3.95) + 0.1*Math.random();
+					var motion = this.settings.motion.value;
+					var heat = r*(avg + Fn.lerp(this.settings.feedback.value, 0.25, motion*motion) * this.heatMaps[2].data[ix]);
+					heat = cooling * heat;
 					if (this.mode) {
 						heat = 1 - heat;
 					}
-					target.data[ix] = cooling*v + heat*heat;
-					var rgba = this.heatToColor(v);
+					target.data[ix] = heat;
+					this.maxHeat = Math.max(this.maxHeat, heat);
+					var rgba = this.heatToColor(heat);
 					this.backBuffer.buffer.data[4*ix] = rgba[0];
 					this.backBuffer.buffer.data[4*ix+1] = rgba[1];
 					this.backBuffer.buffer.data[4*ix+2] = rgba[2];
-					this.backBuffer.buffer.data[4*ix+3] = rgba[3];
+					//this.backBuffer.buffer.data[4*ix+3] = rgba[3];
 					ix++;
 				}
+				jx +=source.width;
 			}
 			this.backBuffer.context.putImageData(this.backBuffer.buffer, 0, 0);
+			//this.frontBuffer.context.clearRect(0, 0, this.frontBuffer.buffer.width, this.frontBuffer.buffer.height);
 			this.frontBuffer.context.drawImage(this.backBuffer.context.canvas, 0, 0, this.backBuffer.buffer.width, this.backBuffer.buffer.height, 0, 0, this.frontBuffer.buffer.width, this.frontBuffer.buffer.height);
 		},
         render: function render(frame, dt) {
 			glui.renderingContext2d.save();
 			glui.renderingContext2d.drawImage(this.frontBuffer.context.canvas, 0, 0, this.frontBuffer.buffer.width, this.frontBuffer.buffer.height, 0, 0, glui.width, glui.height);
 			glui.renderingContext2d.globalAlpha = 0.3;
-			glui.renderingContext2d.font = '8px Consolas';
+			glui.renderingContext2d.font = '14px Consolas';
 			glui.renderingContext2d.fillStyle = "#ffe080";
 			glui.renderingContext2d.textAlign = "left";
 			glui.renderingContext2d.fillText("mode:" + this.mode, 0, glui.height - 2);
+			glui.renderingContext2d.fillText("heat:" + this.maxHeat, 100, glui.height - 2);
 			glui.renderingContext2d.restore();
 		},
-		
 		setImage: function setImage() {
 			var ix = DemoMgr.controls.settings.rows['images'].cells['value'].getValue();
 			var img = this.images[ix].value;
 			if (this.backBuffer.context.canvas.width != img.width || this.backBuffer.context.canvas.height != img.height) {
 				this.backBuffer.context.canvas.width = img.width;
 				this.backBuffer.context.canvas.height = img.height;
+				this.backBuffer.context = this.backBuffer.context.canvas.getContext('2d');
 			}
-			this.backBuffer.context.drawImage(img, 0, 0);
+
+			this.backBuffer.context.drawImage(img, 0, 0, img.width, img.height, 0, 0, img.width, img.height);
 			this.backBuffer.buffer = this.backBuffer.context.getImageData(0, 0, this.backBuffer.context.canvas.width, this.backBuffer.context.canvas.height);
 			this.heatMaps[0] = this.createHeatMap(this.backBuffer.buffer);
 			this.heatMaps[2] = this.createHeatMap(this.backBuffer.buffer);
 		},
 		createHeatMap: function createHeatMap(buffer) {
-			buffer = buffer || this.frontBuffer.buffer;
+			buffer = buffer || this.backBuffer.buffer;
 			var heatMap = {
 				width: buffer.width,
 				height: buffer.height,
@@ -177,27 +191,27 @@ debugger
 			var tbl = [
 				[
 					[1.00, 255, 255, 255, 255],
-					[0.95, 255, 255, 255, 255],
-					[0.80, 255, 240, 128, 255],
-					[0.60, 240,  64,  64, 192],
-					[0.40, 128,  64,  64, 128],
-					[0.10,  64,  64,  64,  64],
-					[0.0,   0,   0,   0,   0]
+					[0.60, 255, 255, 255, 255],
+					[0.36, 255, 240, 128, 255],
+					[0.22, 240,  64,  64, 192],
+					[0.13, 128,  64,  64, 128],
+					[0.07,  64,  64,  64,  64],
+					[0.00,   0,   0,   0,   0]
 				],
 				[
-					[1.0, 255, 255, 255, 255],
-					[0.9, 255, 255, 255, 255],
-					[0.8, 128, 160, 240, 255],
-					[0.7,  64,  64, 192, 128],
-					[0.5,  64,  64,  64,  64],
-					[0.0,   0,   0,   0,   0]
+					[1.00, 255, 255, 255, 255],
+					[0.50, 255, 255, 255, 255],
+					[0.25, 128, 160, 240, 255],
+					[0.12,  64,  64, 192, 128],
+					[0.06,  64,  64,  64,  64],
+					[0.00,   0,   0,   0,   0]
 				]
 			];
 			
 			var col = [0, 0, 0, 0];
 			var tab = tbl[tix];
 			var i0 = tab[0];
-			v *= v;
+			v /= 100;
 			for (var i=1; i<tab.length; i++) {
 				var i1 = tab[i];
 				if (v > i1[0]) {
@@ -212,7 +226,5 @@ debugger
 			return col;
 		}
 	};
-
-
 	public(Fire, 'Fire');
 })();
