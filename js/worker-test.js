765762d8dd7b1b009af1806e537c3a8d247e36af
ng210 <nemeth_g@freemail.hu>
2020-09-25 20:05:13 +0200

    myworker poc

--- js/worker-test.js
+++ js/worker-test.js
@@ -1,11 +1,39 @@
+include('/base/dbg.js');
 include('/synth/synth.js');
+include('/ge/sound.js');
 
 (function() {
-
-    function main() {
-        Dbg.prln('MAIN');
-        self.postMessage({code:'kill', id:0, data:`Worker terminated!`});
+    var counter = 0;
+    async function main(msg) {
+        switch (msg.code) {
+            case 'startup':
+                Dbg.prln('Worker started');
+                count();
+                msg.body = 'Counter started';
+                self.postMessage(msg);
+                break;
+            case 'count':
+                msg.body = counter;
+                self.postMessage(msg);
+                break;
+            case 'kill':
+                Dbg.prln('Worker killed');
+                break;
+        }
+    }
+    
+    function count() {
+        setInterval( () => {
+            counter++;
+            if ((counter % 10) == 0) {
+                self.postMessage({
+                    'code':'count',
+                    'id': new Date().getTime(),
+                    'body': counter+':'
+                });
+            }
+        }, 1000);
     }
 
-    public(main, 'main');
+    publish(main, 'main');
 })();
