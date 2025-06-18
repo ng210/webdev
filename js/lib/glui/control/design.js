63bb6765abc9503e3ce0c7abc1bfff3c36013943
Gabor <31445817+ng210@users.noreply.github.com>
2025-06-18 11:01:01 +0200

    Update: new class hierarchy design and sample range-control

--- js/lib/glui/control/design.js
+++ js/lib/glui/control/design.js
@@ -38,3 +38,52 @@ class RangeWebglControl extends WebglControl {
 }
 
 export { HtmlControl, RangeHtmlControl };
+
+
+
+
+class Control {
+  id, value
+  uiElement
+  dataSource, validEvents
+  
+  constructor(id, data)
+  dataBing(dataSource)
+  async initialize(data)
+  addHandler(event, handler)
+}
+
+class UiElement {
+  control, parent
+  
+  constructor(ctrl)
+  update(frame, dt)
+  render(frame, dt)
+  addHandler(event, handler)
+}
+
+class HtmlElement extends UiElement {
+  elem
+  
+  constructor(id, data)
+  update(frame, dt)
+  render(frame, dt)
+  createElement()
+}
+
+class RangeControl extends Control {
+  min, max, step
+}
+
+class HtmlRangeElement extends UiElement{
+  
+  constructor(ctrl)
+  
+}
+
+let ctrl1 = new RangeControl('ctrl1')
+ctrl1.dataBind(
+  { min:0, max:100,step:1,value:10 })
+ctrl1.uiElement = new HtmlRangeElement(ctrl1)
+ctrl1.update(0, 0)
+ctrl1.render(0, 0)
